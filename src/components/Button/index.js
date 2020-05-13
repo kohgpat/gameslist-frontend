7a33e74972c259056b9f4c89c1a0d3b4dbeeb228
Nikolay Burlov <kohgpat@gmail.com>
2020-05-14 02:32:41 +0800

    Add small variant to Button

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -2,14 +2,27 @@ import React from "react";
 import cn from "classnames";
 import s from "./index.module.css";
 
-const Button = ({ children, disabled, type, className, icon, ...rest }) => {
+const Button = ({
+  children,
+  disabled,
+  type,
+  className,
+  small,
+  icon,
+  ...rest
+}) => {
   const Icon = icon;
 
   return (
     <button
       disabled={disabled}
       type={type}
-      className={cn(s.button, disabled && s.buttonDisabled, className)}
+      className={cn(
+        s.button,
+        disabled && s.buttonDisabled,
+        small && s.buttonSmall,
+        className
+      )}
       {...rest}
     >
       {icon && <Icon className={s.buttonIcon} />}
