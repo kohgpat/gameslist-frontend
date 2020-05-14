1fd2bfe56ce12558bfa2f07696230f0c8fa6768a
Nikolay Burlov <kohgpat@gmail.com>
2020-05-14 13:23:23 +0800

    update fake data

--- src/constants.js
+++ src/constants.js
@@ -2,97 +2,169 @@ export const players = [
   {
     id: 1,
     name: "Alice",
+    stats: {
+      wins: 100,
+    },
   },
   {
     id: 2,
     name: "Bob",
+    stats: {
+      wins: 90,
+    },
   },
   {
     id: 3,
     name: "Mark",
+    stats: {
+      wins: 80,
+    },
   },
   {
     id: 4,
     name: "Walter",
+    stats: {
+      wins: 80,
+    },
   },
   {
     id: 5,
-    name: "Alice",
+    name: "Anna",
+    stats: {
+      wins: 80,
+    },
   },
   {
     id: 6,
-    name: "Bob",
+    name: "Clark",
+    stats: {
+      wins: 75,
+    },
   },
   {
     id: 7,
-    name: "Mark",
+    name: "Kent",
+    stats: {
+      wins: 70,
+    },
   },
   {
     id: 8,
-    name: "Walter",
+    name: "Mike",
+    stats: {
+      wins: 70,
+    },
   },
   {
     id: 9,
-    name: "Alice",
+    name: "John",
+    stats: {
+      wins: 70,
+    },
   },
   {
     id: 10,
-    name: "Bob",
+    name: "Helen",
+    stats: {
+      wins: 60,
+    },
   },
   {
     id: 11,
-    name: "Mark",
+    name: "Owen",
+    stats: {
+      wins: 59,
+    },
   },
   {
     id: 12,
-    name: "Walter",
+    name: "Bryan",
+    stats: {
+      wins: 50,
+    },
   },
   {
     id: 13,
-    name: "Alice",
+    name: "Nick",
+    stats: {
+      wins: 49,
+    },
   },
   {
     id: 14,
-    name: "Bob",
+    name: "Tim",
+    stats: {
+      wins: 47,
+    },
   },
   {
     id: 15,
-    name: "Mark",
+    name: "Elliot",
+    stats: {
+      wins: 45,
+    },
   },
   {
     id: 16,
-    name: "Walter",
+    name: "Jon",
+    stats: {
+      wins: 44,
+    },
   },
   {
     id: 17,
-    name: "Alice",
+    name: "Kenneth",
+    stats: {
+      wins: 40,
+    },
   },
   {
     id: 18,
-    name: "Bob",
+    name: "Ryan",
+    stats: {
+      wins: 39,
+    },
   },
   {
     id: 19,
-    name: "Mark",
+    name: "Nat",
+    stats: {
+      wins: 35,
+    },
   },
   {
     id: 20,
-    name: "Walter",
+    name: "Laurel",
+    stats: {
+      wins: 30,
+    },
   },
   {
     id: 21,
-    name: "Alice",
+    name: "Tom",
+    stats: {
+      wins: 20,
+    },
   },
   {
     id: 22,
-    name: "Bob",
+    name: "Kat",
+    stats: {
+      wins: 19,
+    },
   },
   {
     id: 23,
-    name: "Mark",
+    name: "Kim",
+    stats: {
+      wins: 10,
+    },
   },
   {
     id: 24,
-    name: "Walter",
+    name: "Jason",
+    stats: {
+      wins: 1,
+    },
   },
 ];
