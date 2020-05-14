dfc86cd7e3846855a72ac8bf50756360c032e054
Nikolay Burlov <kohgpat@gmail.com>
2020-05-14 13:59:59 +0800

    Remove selected and onClick from PlayerCard

--- src/components/PlayerCard/index.js
+++ src/components/PlayerCard/index.js
@@ -2,23 +2,12 @@ import React from "react";
 import cn from "classnames";
 import s from "./index.module.css";
 
-const PlayerCard = ({ player, selected, onClick, roundBorders }) => {
+const PlayerCard = ({ player, roundBorders }) => {
   return (
-    <div
-      className={cn(
-        s.player,
-        selected && s.playerSelected,
-        roundBorders && s.playerRoundBorders
-      )}
-      onClick={() => onClick(player)}
-    >
+    <div className={cn(s.player, roundBorders && s.playerRoundBorders)}>
       <span className={s.playerName}>{player.name}</span>
     </div>
   );
-};
-
-PlayerCard.defaultProps = {
-  onClick: () => {},
 };
 
 export default PlayerCard;
