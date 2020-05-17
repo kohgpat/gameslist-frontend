2f07db2ed70c2dd6357f99d980a74ccc2e01416b
Nikolay Burlov <kohgpat@gmail.com>
2020-05-17 16:37:48 +0800

    simplify new game page with useReducer

--- src/pages/NewGame/index.js
+++ src/pages/NewGame/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { FiPlusCircle, FiArrowUpCircle } from "react-icons/fi";
 import { GamesProvider } from "../../contexts/Games";
 import { PlayersProvider } from "../../contexts/Players";
@@ -14,41 +14,120 @@ import ValidationDialog from "../../components/ValidationDialog";
 import { players } from "../../constants";
 import s from "./index.module.css";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "SET_PLAYERS_SELECT_VISIBLE": {
+      return {
+        ...state,
+        playersSelectVisible: action.visible,
+      };
+    }
+    case "SET_VALIDATION_DIALOG_VISIBLE": {
+      return {
+        ...state,
+        validationDialogVisible: action.visible,
+      };
+    }
+    case "ADD_PLAYER_TO_GAME": {
+      if (state.playersInGame.length === 4) {
+        return state;
+      }
+
+      return {
+        ...state,
+        playersInGame: [...state.playersInGame, action.player],
+      };
+    }
+    case "REMOVE_PLAYER_FROM_GAME": {
+      const idx = state.playersInGame.findIndex(
+        (p) => p.id === action.player.id
+      );
+
+      const winner =
+        state.winner?.id === action.player.id ? undefined : state.winner;
+
+      return {
+        ...state,
+        playersInGame: [
+          ...state.playersInGame.slice(0, idx),
+          ...state.playersInGame.slice(idx + 1),
+        ],
+        winner,
+      };
+    }
+    case "SET_WINNER": {
+      return {
+        ...state,
+        winner: action.player,
+      };
+    }
+    default: {
+      throw new Error("Unsupported action: ", action);
+    }
+  }
+};
+
 const NewGame = () => {
-  const [playersSelectVisible, setPlayersSelectVisible] = useState(false);
-  const [validationDialogVisible, setValidationDialogVisible] = useState(false);
-  const [playersInGame, setPlayersInGame] = useState([]);
-  const [winner, setWinner] = useState(undefined);
+  const [state, dispatch] = useReducer(reducer, {
+    playersSelectVisible: false,
+    validationDialogVisible: false,
+    playersInGame: [],
+    winner: undefined,
+  });
 
   const addToGame = (player) => {
-    if (playersInGame.length === 4) {
-      return;
-    }
-
-    setPlayersInGame([...playersInGame, player]);
+    dispatch({ type: "ADD_PLAYER_TO_GAME", player });
   };
 
   const removeFromGame = (player) => {
-    const idx = playersInGame.findIndex((p) => p.id === player.id);
-    setPlayersInGame([
-      ...playersInGame.slice(0, idx),
-      ...playersInGame.slice(idx + 1),
-    ]);
+    dispatch({ type: "REMOVE_PLAYER_FROM_GAME", player });
   };
 
   const togglePlayerInGame = (player) => {
-    if (playersInGame.includes(player)) {
+    if (state.playersInGame.includes(player)) {
       removeFromGame(player);
     } else {
       addToGame(player);
     }
   };
 
+  const togglePlayersSelect = () => {
+    dispatch({
+      type: "SET_PLAYERS_SELECT_VISIBLE",
+      visible: !state.playersSelectVisible,
+    });
+  };
+
+  const setPlayersSelectVisible = (visible) => {
+    dispatch({
+      type: "SET_PLAYERS_SELECT_VISIBLE",
+      visible,
+    });
+  };
+
+  const setValidationDialogVisible = (visible) => {
+    dispatch({
+      type: "SET_VALIDATION_DIALOG_VISIBLE",
+      visible,
+    });
+  };
+
+  const setWinner = (player) => {
+    dispatch({ type: "SET_WINNER", player });
+  };
+
   const submitGame = () => {
-    if (!playersInGame.length || !winner) {
-      setValidationDialogVisible(true);
+    if (!state.playersInGame.length || !state.winner) {
+      dispatch({ type: "SET_VALIDATION_DIALOG_VISIBLE", visible: true });
     }
   };
+
+  const {
+    playersInGame,
+    winner,
+    playersSelectVisible,
+    validationDialogVisible,
+  } = state;
 
   return (
     <Screen>
@@ -63,17 +142,13 @@ const NewGame = () => {
             </Title>
 
             {!!playersInGame.length ? (
-              <Button
-                type="button"
-                small
-                onClick={() => setPlayersSelectVisible(!playersSelectVisible)}
-              >
+              <Button type="button" small onClick={togglePlayersSelect}>
                 Change
               </Button>
             ) : (
               <IconButton
                 type="button"
-                onClick={() => setPlayersSelectVisible(!playersSelectVisible)}
+                onClick={togglePlayersSelect}
                 disabled={playersInGame.length === 4}
               >
                 <FiPlusCircle className={s.addPlayerButtonIcon} />
