c13595e11c4218ca2843d72c460e2bf2effcdbec
Nikolay Burlov <kohgpat@gmail.com>
2020-05-20 00:46:16 +0800

    Wrap Input with forwardRef

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import cn from "classnames";
 import s from "./index.module.css";
 
-const Input = ({ type, value, onChange, className, ...rest }) => {
-  return (
+const Input = forwardRef(
+  ({ type, value, onChange, className, ...rest }, ref) => (
     <input
       className={cn(s.input, className)}
       type={type}
       value={value}
       onChange={onChange}
+      ref={ref}
       {...rest}
     />
-  );
-};
+  )
+);
 
 export default Input;
