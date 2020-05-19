58deb58f2d160a436e31880fe0ada0ff64003b54
Nikolay Burlov <kohgpat@gmail.com>
2020-05-20 00:46:28 +0800

    Accept initialFocusRef to pass into reach-ui Dialog

--- src/components/Dialog/index.js
+++ src/components/Dialog/index.js
@@ -3,9 +3,13 @@ import { DialogOverlay, DialogContent } from "@reach/dialog";
 import "@reach/dialog/styles.css";
 import s from "./index.module.css";
 
-const Dialog = ({ isVisible, onDismiss, label, children }) => {
+const Dialog = ({ isVisible, onDismiss, initialFocusRef, label, children }) => {
   return (
-    <DialogOverlay isOpen={isVisible} onDismiss={onDismiss}>
+    <DialogOverlay
+      isOpen={isVisible}
+      onDismiss={onDismiss}
+      initialFocusRef={initialFocusRef}
+    >
       <DialogContent className={s.dialogContent} aria-label={label}>
         {children}
       </DialogContent>
