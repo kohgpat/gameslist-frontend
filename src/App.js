34e036221588a711820f29e7a5025abb119f8548
Nikolay Burlov <kohgpat@gmail.com>
2020-05-28 18:41:04 +0800

    Extract routes into Router component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,13 @@
 import React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
 import { FlashMessageProvider } from "./contexts/FlashMessage";
 import FlashMessage from "./components/FlashMessage";
-import Index from "./pages/Index";
-import Players from "./pages/Players";
-import PlayersShow from "./pages/PlayersShow";
-import Games from "./pages/Games";
-import NewGame from "./pages/NewGame";
-import Rating from "./pages/Rating";
+import Router from "./Router";
 
 function App() {
   return (
     <FlashMessageProvider>
       <FlashMessage />
-
-      <BrowserRouter>
-        <Switch>
-          <Route path="/new-game">
-            <NewGame />
-          </Route>
-
-          <Route path="/players/:id">
-            <PlayersShow />
-          </Route>
-
-          <Route path="/players">
-            <Players />
-          </Route>
-
-          <Route path="/games">
-            <Games />
-          </Route>
-
-          <Route path="/rating">
-            <Rating />
-          </Route>
-
-          <Route path="/">
-            <Index />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      <Router />
     </FlashMessageProvider>
   );
 }
---
+++ src/Router.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import Players from "./pages/Players";
+import PlayersShow from "./pages/PlayersShow";
+import Games from "./pages/Games";
+import NewGame from "./pages/NewGame";
+import Rating from "./pages/Rating";
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/new-game">
+          <NewGame />
+        </Route>
+
+        <Route path="/players/:id">
+          <PlayersShow />
+        </Route>
+
+        <Route path="/players">
+          <Players />
+        </Route>
+
+        <Route path="/games">
+          <Games />
+        </Route>
+
+        <Route path="/rating">
+          <Rating />
+        </Route>
+
+        <Route path="/">
+          <Index />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
