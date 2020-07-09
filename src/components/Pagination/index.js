7fafb660400a634eacb5f46810f2ad822d9eaae3
Nikolay Burlov <kohgpat@gmail.com>
2020-07-09 23:49:30 +0800

    update pagination

--- src/components/Pagination/index.js
+++ src/components/Pagination/index.js
@@ -1,23 +1,32 @@
 import React from "react";
+import cn from "classnames";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 import s from "./index.module.css";
 
 const Pagination = ({ page, pageSize, hasMore, onPageSelect }) => {
+  const prevButtonDisabled = page === 0;
+  const nextButtonDisabled = !hasMore;
+
   return (
     <div className={s.pagination}>
       <button
         type="button"
+        className={cn(s.control, prevButtonDisabled && s.controlDisabled)}
         onClick={() => onPageSelect(page - 1)}
-        disabled={page === 0}
+        disabled={prevButtonDisabled}
       >
-        {"<"}
+        <FiChevronLeft />
       </button>
+
       <div className={s.page}>{page + 1}</div>
+
       <button
         type="button"
+        className={cn(s.control, nextButtonDisabled && s.controlDisabled)}
         onClick={() => onPageSelect(page + 1)}
-        disabled={!hasMore}
+        disabled={nextButtonDisabled}
       >
-        {">"}
+        <FiChevronRight />
       </button>
     </div>
   );
